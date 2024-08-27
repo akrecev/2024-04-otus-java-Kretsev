4edd60d49ad1f62cb5fbe9f5fc9946b5448befe1
askretsev <kretsev@yandex.ru>
2024-08-27 14:21:35 +0300

    refactor migration

--- hw21-jpql/homework-template/src/main/resources/db/migration/V1__initial_schema.sql
+++ hw21-jpql/homework-template/src/main/resources/db/migration/V1__initial_schema.sql
@@ -9,10 +9,36 @@ create table client
  */
 
 -- Для @GeneratedValue(strategy = GenerationType.SEQUENCE)
-create sequence client_SEQ start with 1 increment by 1;
+-- create sequence client_SEQ start with 1 increment by 1;
+--
+-- create table client
+-- (
+--     id   bigint not null primary key,
+--     name varchar(50)
+-- );
 
+create sequence address_seq start with 1 increment by 1;
+create sequence client_seq start with 1 increment by 1;
+create sequence phone_seq start with 1 increment by 1;
+create table address
+(
+    address_owner_id bigint unique,
+    id               bigint not null,
+    street           varchar(255),
+    primary key (id)
+);
 create table client
 (
-    id   bigint not null primary key,
-    name varchar(50)
+    id   bigint not null,
+    name varchar(255),
+    primary key (id)
 );
+create table phone
+(
+    id             bigint not null,
+    phone_owner_id bigint,
+    number         varchar(255),
+    primary key (id)
+);
+alter table if exists address add constraint fk_address_client foreign key (address_owner_id) references client;
+alter table if exists phone add constraint fk_phone_client foreign key (phone_owner_id) references client;
