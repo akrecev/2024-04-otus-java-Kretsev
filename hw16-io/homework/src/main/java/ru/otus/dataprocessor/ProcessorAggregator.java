ba5bd01f72495b6e2f847594d7524cefe35190b2
akrecev <akrecev@gmail.com>
2024-08-31 20:14:13 +0300

    add hw-16

--- hw16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -1,15 +1,16 @@
 package ru.otus.dataprocessor;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import ru.otus.model.Measurement;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
-        // группирует выходящий список по name, при этом суммирует поля value
-        return Collections.emptyMap();
+        Map<String, Double> result = new TreeMap<>();
+        for (Measurement datum : data) {
+            result.compute(datum.name(), (k, v) -> v == null ? datum.value() : v + datum.value());
+        }
+        return result;
     }
 }
