920cffc2efd8374f36747edf7cf26c4b8eecc5ad
askretsev <kretsev@yandex.ru>
2024-09-10 13:22:27 +0300

    add dbapp on hw22 module

---
+++ hw22-cache/src/main/java/ru/otus/dbapp/crm/model/Phone.java
@@ -0,0 +1,60 @@
+package ru.otus.dbapp.crm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "phone")
+@SuppressWarnings({"java:S1182", "java:S2975"})
+public class Phone {
+
+    @Id
+    @SequenceGenerator(name = "phone_gen", sequenceName = "phone_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "phone_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "phone_owner_id", foreignKey = @ForeignKey(name = "fk_phone_client"))
+    private Client phoneOwner;
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    @Override
+    public Phone clone() {
+        return new Phone(this.id, this.number, this.phoneOwner);
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{id=" + id + ", number='" + number + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Phone phone = (Phone) o;
+        return Objects.equals(id, phone.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ hw22-cache/src/main/java/ru/otus/dbapp/crm/service/DBServiceClient.java
@@ -0,0 +1,16 @@
+package ru.otus.dbapp.crm.service;
+
+
+import ru.otus.dbapp.crm.model.Client;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DBServiceClient {
+
+    Client saveClient(Client client);
+
+    Optional<Client> getClient(long id);
+
+    List<Client> findAll();
+}
