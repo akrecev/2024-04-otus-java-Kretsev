ffe833e72b3b93824e77638c5c02651d1997307c
askretsev <kretsev@yandex.ru>
2024-09-10 13:59:48 +0300

    add MyCache realisation

--- hw22-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw22-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,30 +1,57 @@
 package ru.otus.cachehw;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 public class MyCache<K, V> implements HwCache<K, V> {
     // Надо реализовать эти методы
+    private static final Logger logger = LoggerFactory.getLogger(MyCache.class);
+
+    private final Map<K, V> cache;
+    private final List<HwListener<K, V>> listeners;
+
+    public MyCache() {
+        this.cache = new WeakHashMap<>();
+        this.listeners = new ArrayList<>();
+    }
 
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        cache.put(key, value);
+        listeners.forEach(listener -> listener.notify(key, value, "updated"));
+        logger.info("Cache size after addition: {}", cache.size());
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        V removed = cache.remove(key);
+        listeners.forEach(listener -> listener.notify(key, removed, "removed"));
+        logger.info("Cache size after removing: {}", cache.size());
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        V gotten = cache.get(key);
+        listeners.forEach(listener -> listener.notify(key, gotten, "get"));
+        logger.info("Getting from cache: {}", gotten.toString());
+
+        return gotten;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.add(listener);
+        logger.info("Listener added: {}", listener.getClass().getName());
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.remove(listener);
+        logger.info("Listener removed: {}", listener.getClass().getName());
     }
 }
