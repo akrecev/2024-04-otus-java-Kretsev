3f76d79f180b5fd39f01a38683a5896c09df6afa
askretsev <kretsev@yandex.ru>
2024-09-11 08:19:27 +0300

    add tests from 21 module

--- hw22-cache/build.gradle.kts
+++ hw22-cache/build.gradle.kts
@@ -7,4 +7,13 @@ dependencies {
     implementation("org.hibernate.orm:hibernate-core")
     implementation("org.flywaydb:flyway-core")
     implementation("org.postgresql:postgresql")
+
+    testImplementation("com.h2database:h2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.mockito:mockito-junit-jupiter")
+
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
 }
