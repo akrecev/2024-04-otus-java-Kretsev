d452ebfe16a3d21f21f7371ce228ad969cb19601
akrecev <akrecev@gmail.com>
2025-05-02 18:15:52 +0300

    add repositories

---
+++ hw28-springDataJdbc/homework/src/main/java/ru/otus/repository/PhoneRepository.java
@@ -0,0 +1,7 @@
+package ru.otus.repository;
+
+import org.springframework.data.repository.ListCrudRepository;
+import ru.otus.model.Phone;
+
+public interface PhoneRepository extends ListCrudRepository<Phone, Long> {
+}
