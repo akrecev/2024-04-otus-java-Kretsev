80374069b079eeea1f928bf4a04b7dd210a65204
akrecev <akrecev@gmail.com>
2025-05-14 20:59:38 +0300

    Monitor's realisation on homework

---
+++ hw31-executors/src/main/java/ru/otus/monitor/MonitorHomework.java
@@ -0,0 +1,62 @@
+package ru.otus.monitor;
+
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MonitorHomework {
+    private static final Logger log = LoggerFactory.getLogger(MonitorHomework.class);
+    private static final int TOP = 10;
+    private static final int DELAY_ON_SECONDS = 1;
+    private static final String FIRST_THREAD_NAME = "Thread-1";
+    private static final String SECOND_THREAD_NAME = "Thread-2";
+    private static boolean isThread1Turn = true;
+    private final Object lock = new Object();
+
+    public static void main(String[] args) {
+        var pusher = new MonitorHomework();
+
+        new Thread(() -> pusher.push(FIRST_THREAD_NAME, true)).start();
+        new Thread(() -> pusher.push(SECOND_THREAD_NAME, false)).start();
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(DELAY_ON_SECONDS));
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void push(String name, boolean isThreadFirst) {
+        synchronized (lock) {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    for (int i : generateSequence()) {
+                        while (isThread1Turn != isThreadFirst) {
+                            lock.wait();
+                        }
+                        sleep();
+                        log.info("{}: {}", name, i);
+                        isThread1Turn = !isThread1Turn;
+                        lock.notifyAll();
+                    }
+                    return;
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    private int[] generateSequence() {
+        int[] array = new int[MonitorHomework.TOP * 2 - 1];
+        for (int i = 0; i < MonitorHomework.TOP; i++) {
+            array[i] = i + 1;
+            array[array.length - i - 1] = i + 1;
+        }
+
+        return array;
+    }
+}
