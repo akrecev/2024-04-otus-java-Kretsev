3e0ce0144c2d35312822b2a67b9af0c255b45545
Andrey Kretsev <akrecev@gmail.com>
2025-05-25 15:23:02 +0300

    add synchronized realisation SensorDataProcessorBuffered

--- hw32-concurrentCollections/QueueHomework/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw32-concurrentCollections/QueueHomework/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -1,5 +1,9 @@
 package ru.otus.services.processors;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.PriorityBlockingQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.api.SensorDataProcessor;
@@ -13,24 +17,37 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private final PriorityBlockingQueue<SensorData> dataBuffer;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.dataBuffer = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-        /*
-            if (dataBuffer.size() >= bufferSize) {
-                flush();
-            }
-        */
+        dataBuffer.add(data);
+        if (dataBuffer.size() >= bufferSize) {
+            flush();
+        }
     }
 
-    public void flush() {
+    public synchronized void flush() {
         try {
-            // writer.writeBufferedData(bufferedData);
+            List<SensorData> bufferedData = new ArrayList<>();
+            if (dataBuffer.isEmpty()) {
+                return;
+            }
+
+            SensorData polled;
+            while ((polled = dataBuffer.poll()) != null) {
+                bufferedData.add(polled);
+            }
+
+            if (!bufferedData.isEmpty()) {
+                writer.writeBufferedData(bufferedData);
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
