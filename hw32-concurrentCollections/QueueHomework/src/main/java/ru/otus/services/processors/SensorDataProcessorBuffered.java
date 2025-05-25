8ad5071b0b3c3e4935b56e0389395a780104efb6
Andrey Kretsev <akrecev@gmail.com>
2025-05-25 20:49:19 +0300

    fix: only flush method lock

--- hw32-concurrentCollections/QueueHomework/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw32-concurrentCollections/QueueHomework/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -30,37 +30,14 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     @Override
     public void process(SensorData data) {
-        lock.lock();
-        try {
-            dataBuffer.add(data);
-            if (dataBuffer.size() >= bufferSize) {
-                flush();
-            }
-        } finally {
-            lock.unlock();
+        dataBuffer.add(data);
+        if (dataBuffer.size() >= bufferSize) {
+            flush();
         }
     }
 
     public void flush() {
         lock.lock();
-        try {
-            flushInternal();
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    @Override
-    public void onProcessingEnd() {
-        lock.lock();
-        try {
-            flush();
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    private void flushInternal() {
         try {
             List<SensorData> bufferedData = new ArrayList<>();
             if (dataBuffer.isEmpty()) {
@@ -77,6 +54,13 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
             }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
+        } finally {
+            lock.unlock();
         }
+    }
+
+    @Override
+    public void onProcessingEnd() {
+        flush();
     }
 }
